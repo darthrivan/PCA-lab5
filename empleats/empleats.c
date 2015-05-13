0e02628cbed9796950143feadf17a3a086956066
Víctor Mas <victor.mas@est.fib.upc.edu>
2015-05-13 22:40:23 +0200

    created auxiliar light structure to order

--- empleats/empleats.c
+++ empleats/empleats.c
@@ -25,19 +25,28 @@ typedef struct
 #endif
 } Templeat;
 
+typedef struct
+{
+	long long int NID;
+	Templeat *data;
+} Templeat_nids;
+
 int compare(const void *p1, const void *p2)
 {
-    return ((Templeat*) p1)->NID - ((Templeat*) p2)->NID;
+    return ((Templeat_nids*) p1)->NID - ((Templeat_nids*) p2)->NID;
 }
 
 int main(int argc, char *argv[])
 {
-	Templeat *empleats;
+	Templeat *empleats, *empleats_def;
+	Templeat_nids *ids;
 	unsigned int N = 4000000, i;
 
 	if (argc > 1) N = atoi(argv[1]);
 
+	ids = (Templeat_nids *) malloc(N*sizeof(Templeat_nids));
 	empleats = (Templeat *) malloc(N*sizeof(Templeat));
+	empleats_def = (Templeat *) malloc(N*sizeof(Templeat));
 	if (empleats == NULL) { fprintf(stderr, "Out of memory\n"); exit(0); }
 	memset(empleats, 0, N*sizeof(Templeat));
 
@@ -50,18 +59,23 @@ int main(int argc, char *argv[])
 		empleats[i].Categoria[i % MAX_CATEGORIA] = (i << 2) * i;
 		empleats[i].ptrClauForaneaDepartament = i<<10 + i<<14 + i>>3;
 		empleats[i].ptrClauCategoria = i<<24 + i<< 18 + i<<10 + i>>2;
+		ids[i].NID = empleats[i].NID;
+		ids[i].data = &empleats[i];
 #ifdef EXTES     
-                empleats[i].Address[0] = 'A' + ((i>>4) + (i<<2)) & 0x15;
-                empleats[i].Nom_parella[0] = 'a' + ((i>>5) + (i<<2)) & 0x15;
+        empleats[i].Address[0] = 'A' + ((i>>4) + (i<<2)) & 0x15;
+        empleats[i].Nom_parella[0] = 'a' + ((i>>5) + (i<<2)) & 0x15;
   		empleats[i].Cognoms_parella[0] = 'A' + ((i>>5) + (i<<3)) & 0x15;
 #endif
 	}
 
-	qsort(empleats, N, sizeof(Templeat), compare);
+	qsort(ids, N, sizeof(Templeat_nids), compare);
+	for (i=0; i < N; ++i) {
+		empleats_def[i] = *(ids[i].data);
+	}
 	unsigned int N_blocks = N / BUFFER_SIZE;
 	for (i=0; i<N_blocks*BUFFER_SIZE; i += BUFFER_SIZE){
-		write (1, &empleats[i], BUFFER_SIZE*sizeof(Templeat));
+		write (1, &empleats_def[i], BUFFER_SIZE*sizeof(Templeat));
 	}
-	write (1, &empleats[i], (N % BUFFER_SIZE)*sizeof(Templeat));
+	write (1, &empleats_def[i], (N % BUFFER_SIZE)*sizeof(Templeat));
 	return 0;
 }
